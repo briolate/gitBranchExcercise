ab5cef66a4df6555f340e803742c8ae3d0a41873
Evan Briolat <briolate@gmail.com>
2017-05-09 13:30:18 -0400

    Added script file

---
+++ script.js
@@ -0,0 +1,11 @@
+for(var i = 0; i < 30; i++) {
+  if(i % 5 === 0 && i % 3 ===0) {
+    console.log('fizzbuzz');
+  } else if(i % 5 === 0) {
+    console.log('buzz');
+  } else if(i % 3 === 0) {
+    console.log('fiz');
+  } else {
+    console.log(i);
+  }
+}
