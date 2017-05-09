9ee9b34dbda364f55d06bc3081adc69bd214ee10
Evan Briolat <briolate@gmail.com>
2017-05-09 13:47:23 -0400

    Deleted palindome from master

--- script.js
+++ script.js
@@ -9,12 +9,3 @@ for(var i = 0; i < 30; i++) {
     console.log(i);
   }
 }
-
-function isPal(word) {
-  var ogWOrd = word;
-  if(ogWord === word.split('').reverse().join('')) {
-    console.log('ya it is');
-  } else {
-    console.log('no');
-  }
-}
